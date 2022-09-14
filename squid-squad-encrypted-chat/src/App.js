18ebffd1513cc122ce9ebf91ad249f3d2d1addcd
byron <byrondalberg@gmail.com>
2022-09-14 06:51:52 -0700

    this is totally a legitimite app

---
+++ squid-squad-encrypted-chat/src/App.js
@@ -0,0 +1,113 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBzhdnfSrRQZmUS8Q34HVJ4LT7QqktQSNY",
+  authDomain: "squid-squad-chatter.firebaseapp.com",
+  projectId: "squid-squad-chatter",
+  storageBucket: "squid-squad-chatter.appspot.com",
+  messagingSenderId: "260677747039",
+  appId: "1:260677747039:web:05d64ed4f99e0a22beb6ea",
+  measurementId: "G-66PVNC6MGN"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🦑 Squid Squad Chatter 🦑</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Offensive Content Only</p>
+    </>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+        <button type="submit" disabled={!formValue}>🦑</button>
+      </form>
+    </>
+  )
+}
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://pyxis.nymag.com/v1/imgs/2ab/efd/cfef23e241d9d4aee7461c70369e54d44d-15-google-eye-cuttlefish.2x.rhorizontal.w710.jpg'} />
+      <p>{text}</p>
+    </div>
+  )
+}
+export default App;
